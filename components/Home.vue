777d16a84b960e68b935714b06b4d26d3f8a1c8e
rmaniego <rodney.maniegojr@gmail.com>
2024-03-05 21:44:25 +0800

    Add copyright

---
+++ components/Home.vue
@@ -0,0 +1,73 @@
+<script lang="ts">
+/* https://docs.aws.amazon.com/prescriptive-guidance/latest/best-practices-cdk-typescript-iac/typescript-best-practices.html */
+/* https://github.com/bevacqua/dragula/issues/602#issuecomment-477734315 */
+if (typeof (window as any).global === "undefined") {
+  (window as any).global = window;
+}
+</script>
+
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <client-only>
+    <div class="az-main">
+      <div class="copyright">&copy; 2024 <a href="https://github.com/rmaniego/" class="profile leckerli-one">Rydeon <span class="emoji1">⚡</span><span class="emoji2">✨</span></a></div>
+    </div>
+  </client-only>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+.copyright {
+	position: fixed;
+  left: 0;
+	bottom: 0;
+	padding: 10px; 
+	width: 100%;
+	color: rgba(255, 255, 255, 0.7);
+  font-family: Arial, Helvetica, sans-serif;
+	text-align: center;
+	font-size: 0.7em;
+}
+
+.profile {
+	padding: 3px 5px 3px 8px;
+	color: #fff;
+	background-color: #382f66;
+  font-family: "Leckerli One", cursive;
+  font-weight: 400;
+  font-style: normal;
+	text-decoration: none;
+	border-radius: 8px;
+}
+
+.profile:hover {
+	color: #111 !important;
+	background-color: #e1b403 !important;
+	-webkit-box-shadow: 0px 0px 5px 0px rgba(204,180,0,1);
+	-moz-box-shadow: 0px 0px 5px 0px rgba(204,180,0,1);
+	box-shadow: 0px 0px 5px 0px rgba(204,180,0,1);
+	transition: color 0.5, background-color 0.5s, box-shadow 0.5s;
+}
+
+.emoji1 {
+	margin-left: -3px;
+}
+
+.emoji2 {
+	margin-left: -6px;
+}
+
+@media only screen and (max-width: 500px) {
+
+  .copyright {
+    margin-bottom: 25px;
+  }
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <Html lang="en">
+    <Head>
+      <Title>AnZen | IT Security Notes</Title>
+      <Meta name="viewport" :content="`width=device-width, initial-scale=1`" />
+      <Meta
+        name="description"
+        :content="`IT Security Notes`"
+      />
+      <Meta
+        name="keywords"
+        content="AnZen, anzen, IT security, cybersecurity, threats, vulnerabilities, hacking"
+      />
+      <Link rel="icon" type="image/png" href="/samurai.png"></Link>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true">
+      <link href="https://fonts.googleapis.com/css2?family=Micro+5&family=Roboto:ital,wght@0,100..900;1,100..900&family=Leckerli+One&display=swap" rel="stylesheet">
+      <link href="/reset.css" rel="stylesheet">
+    </Head>
+  </Html>
+  <client-only>
+    <Home />
+  </client-only>
+</template>
